ed4e3bba1dfda097d3aac7fcb92133aeb871e198
ya zhou <huyazhou@msn.com>
2026-02-02 14:28:35 +0800

    feat(component-generator): add preview components and configurations

---
+++ packages/component-generator/src/preview/CrdTable.tsx
@@ -0,0 +1,14 @@
+export { crdTablePageConfig } from "./crdTablePageConfig.js";
+export { workspaceTablePageConfig } from "./workspaceTablePageConfig.js";
+export {
+  CrdColumnsDataSource,
+  CrdPageStoreDataSource,
+  CrdRuntimeParamsDataSource,
+  CrdProjectSelectDataSource,
+  WorkspaceProjectSelectDataSource,
+  MergeParamsDataSource,
+  CrdStoreDataSource,
+  CrdStoreFactoryDataSource,
+  CrdPageContextDataSource,
+} from "./dataSourceDef.js";
+export { CrdTableNode } from "./nodeDef.js";
