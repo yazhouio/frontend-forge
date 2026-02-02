0862846b98f9b8b2ca22c3f3483575bbb555b735
ya zhou <huyazhou@msn.com>
2026-02-02 09:58:50 +0800

    feat(interfaces): add RuntimeContextInfo interface for runtime context management

--- packages/component-generator/src/index.ts
+++ packages/component-generator/src/index.ts
@@ -15,3 +15,4 @@ export type {
 export { CodeGenerator, DataSourceRegistry, Engine, NodeRegistry, SchemaValidator } from "./engine/index.js";
 export type { CodeFragment, DataSourceDefinition, NodeDefinition } from "./engine/interfaces.js";
 export type { PageConfig } from "./engine/JSONSchema.js";
+export type { RuntimeContextInfo } from "./interfaces.js";
